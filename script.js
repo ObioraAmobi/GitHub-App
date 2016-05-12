031c7dcd5bcf9f298932f036728bffc281ebede5
Obiora Amobi <obiora.amobi@gmail.com>
2016-05-12 14:03:43 +0100

    user info

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function(){
   $('#searchButton').on('click', function(e){
     e.preventDefault();
     
-    var username = $('#username').val();
+    var username = $('#usernameInput').val();
     var requesturl   = 'https://api.github.com/users/'+username;
     var reposurl  = 'https://api.github.com/users/'+username+'/repos';
     var starurl  = 'https://api.github.com/users/'+username+'/starred';
@@ -10,7 +10,7 @@ $(function(){
     requestJSON(requesturl, function(json) {
       if(json.message == "Not Found" || username == '') {
 
-        $('#info').html("<p class='errorMessage'>Does not exist</p>");
+        $('#info').html("<div class='errorMessage'>Does not exist</div>");
       }
 
       
@@ -21,10 +21,13 @@ $(function(){
         var bio = json.bio;
         var avatarUrl = json.avatar_url;
         var profileUrl = json.html_url;
-        
-        var outhtml = '<div class="row"><div class="col-md-4"><a href="'+profileUrl+'" target="_blank"><img src="'+avatarUrl+'"></a></div><div class="col-md-8">@'+username+'<br><h2>'+fullname+'</h2><br>'+bio+'</div></div>';
-        // outhtml = outhtml + '<div class="row">';
 
+        if (bio == null) {
+          bio = "this is the bio...";
+        };
+        
+        var guiHtml = '<div class="userInfo"><a href="'+profileUrl+'" target="_blank"><img src="'+avatarUrl+'"></a><div class="userDetails"><i>@'+username+'</i><br><span class="userFullname">'+fullname+'</span><br><span class="bio">'+bio+'</span></div></div>';
+        guiHtml = guiHtml + '<div class="row">';
 
         var repositories;
         $.getJSON(reposurl, function(json){
@@ -34,16 +37,16 @@ $(function(){
         
         function outputPageContent() {
           if(repositories.length == 0) { 
-            outhtml = outhtml + '<div class="row"><div class="col-md-12"><p>'+username+' has no repositories!</p></div></div>'; 
+            guiHtml = guiHtml + '<div class="row"><div class="col-md-12"><p>'+username+' has no repositories!</p></div></div>'; 
           }
           else {
-            outhtml = outhtml + '<div class="row"><div class="col-md-12 repo"><h3>Repositories</h3></div>';
+            guiHtml = guiHtml + '<div class="row"><div class="col-md-12 repo"><h3>Repositories</h3></div>';
             $.each(repositories, function(index) {
-              outhtml = outhtml + '<div class="col-md-9"><p class="repoName"><strong>'+repositories[index].name+'</strong></p></div><div class="col-md-3"><i class="fa fa-star"></i> '+repositories[index].stargazers_count+' <i class="fa fa-code-fork"></i> '+repositories[index].forks_count+'</div><hr>';
+              guiHtml = guiHtml + '<div class="col-md-9"><p class="repoName"><strong>'+repositories[index].name+'</strong></p></div><div class="col-md-3"><i class="fa fa-star"></i> '+repositories[index].stargazers_count+' <i class="fa fa-code-fork"></i> '+repositories[index].forks_count+'</div><hr>';
             });
-            outhtml = outhtml + '</div>';
+            guiHtml = guiHtml + '</div>';
           }
-          $('#info').html(outhtml);
+          $('#info').html(guiHtml);
         } // end outputPageContent()
       } // end else statement
     }); // end requestJSON Ajax call
